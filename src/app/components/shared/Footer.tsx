75598c20332ff4c851ff2543b45164f6618a80b3
godwinolle <godwinolle@gmail.com>
2026-01-11 03:12:02 -0500

    updated footer

--- src/app/components/shared/Footer.tsx
+++ src/app/components/shared/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <footer className={`${cormorantInfant.className} flex flex-col py-10 px-4 text-center`}>
             <p className="text-base">
                 Built with dedication by{" "}
-                <a href="https://rod-syntax.vercel.app/" target="_blank" className="font-bold">
+                <a href="https://rodsyntax.com/" target="_blank" className="font-bold">
                     rod.syntax
                 </a>
             </p>
